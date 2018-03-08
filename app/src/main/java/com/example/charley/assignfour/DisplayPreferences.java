0f9bcc9275e461308a6938c2487c9b29ed35d82c
charley <diamonddynamo96@gmail.com>
2018-03-07 21:38:48 -0500

    Initial commit

---
+++ app/src/main/java/com/example/charley/assignfour/DisplayPreferences.java
@@ -0,0 +1,93 @@
+package com.example.charley.assignfour;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class DisplayPreferences extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_preferences);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+
+        ArrayList<String> list = new ArrayList<>();
+        if(preferences.contains("in1"))
+            list.add(preferences.getString("in1", ""));
+        if(preferences.contains("in2"))
+            list.add(preferences.getString("in2", ""));
+        if(preferences.contains("in2"))
+            list.add(preferences.getString("in2", ""));
+        if(preferences.contains("in2"))
+            list.add(preferences.getString("in2", ""));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.row_item, R.id.listItem, list);
+
+        final ListView listView = findViewById(android.R.id.list);
+        listView.setAdapter(adapter);
+
+        TextView emptyView = findViewById(android.R.id.empty);
+        listView.setEmptyView(emptyView);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) listView.getItemAtPosition(position);
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(getApplicationContext(), selectedItem, duration);
+                toast.show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_for_screens, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.menuAct1){
+            Intent intent = new Intent(getApplicationContext(), UserPreferences.class);
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.menuAct3) {
+            SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.clear();
+            editor.apply();
+
+            ArrayList<String> list = new ArrayList<>();
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.row_item, R.id.listItem, list);
+            final ListView listView = findViewById(android.R.id.list);
+            listView.setAdapter(adapter);
+            TextView emptyView = findViewById(android.R.id.empty);
+            listView.setEmptyView(emptyView);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/com/example/charley/assignfour/UserPreferences.java
@@ -0,0 +1,91 @@
+package com.example.charley.assignfour;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class UserPreferences extends AppCompatActivity {
+
+    EditText in1, in2, in3, in4;
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_preferences);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        in1 = findViewById(R.id.userIn1);
+        in2 = findViewById(R.id.userIn2);
+        in3 = findViewById(R.id.userIn3);
+        in4 = findViewById(R.id.userIn4);
+
+        submit = findViewById(R.id.saveBut);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                SharedPreferences prefs = getSharedPreferences("preferences", MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                if(in1.getText().toString().trim().length() > 0)
+                    editor.putString("in1", in1.getText().toString());
+                if(in2.getText().toString().trim().length() > 0)
+                    editor.putString("in2", in2.getText().toString());
+                if(in3.getText().toString().trim().length() > 0)
+                    editor.putString("in3", in3.getText().toString());
+                if(in4.getText().toString().trim().length() > 0)
+                    editor.putString("in4", in4.getText().toString());
+
+                editor.apply();
+
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(getApplicationContext(), "Data Saved", duration);
+                toast.show();
+
+                in1.setText("");
+                in2.setText("");
+                in3.setText("");
+                in4.setText("");
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_for_screens, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.menuAct2){
+            Intent intent = new Intent(getApplicationContext(), DisplayPreferences.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if(id == R.id.menuAct3){
+            SharedPreferences preferences = getSharedPreferences("preferences",MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.clear();
+            editor.apply();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
