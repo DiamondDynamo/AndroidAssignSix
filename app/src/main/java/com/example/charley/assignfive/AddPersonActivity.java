f5b05113477b7b55b5f39597f281aa824a357d48
Charley Bein <diamonddynamo96@gmail.com>
2018-03-28 15:37:22 -0400

    Code Complete

--- app/src/main/java/com/example/charley/assignfive/AddPersonActivity.java
+++ app/src/main/java/com/example/charley/assignfive/AddPersonActivity.java
@@ -1,15 +1,22 @@
 package com.example.charley.assignfive;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.telephony.PhoneNumberUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,39 +25,59 @@ import java.util.ArrayList;
 
 public class AddPersonActivity extends AppCompatActivity {
 
+    EditText fName, lName, email,phone;
+    Button submit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_db);
+        setContentView(R.layout.activity_add_person);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
 
-        ArrayList<String> list = new ArrayList<>();
-        if(preferences.contains("fName"))
-            list.add(preferences.getString("fName", ""));
-        if(preferences.contains("lName"))
-            list.add(preferences.getString("lName", ""));
-        if(preferences.contains("lName"))
-            list.add(preferences.getString("email", ""));
-        if(preferences.contains("lName"))
-            list.add(preferences.getString("phone", ""));
+        fName = findViewById(R.id.fName);
+        lName = findViewById(R.id.lName);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.row_item, R.id.listItem, list);
+        phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+        submit = findViewById(R.id.subBut);
 
-        final ListView listView = findViewById(android.R.id.list);
-        listView.setAdapter(adapter);
-
-        TextView emptyView = findViewById(android.R.id.empty);
-        listView.setEmptyView(emptyView);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItem = (String) listView.getItemAtPosition(position);
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(getApplicationContext(), selectedItem, duration);
+            public void onClick(View v) {
+                PersonDbHelper myDbHelper = new PersonDbHelper(getApplicationContext());
+                SQLiteDatabase db = myDbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+
+                values.put(PersonContract.PersonEntry.COLUMN_NAME_FIRST, fName.getText().toString());
+                values.put(PersonContract.PersonEntry.COLUMN_NAME_LAST, lName.getText().toString());
+                values.put(PersonContract.PersonEntry.COLUMN_EMAIL, email.getText().toString());
+                values.put(PersonContract.PersonEntry.COLUMN_PHONE, PhoneNumberUtils.formatNumber(phone.getText().toString(), "US"));
+
+                long newRowId = db.insert(PersonContract.PersonEntry.TABLE_NAME, null, values);
+
+                int duration = Toast.LENGTH_LONG;
+                String result;
+
+                if (newRowId != -1)
+                {
+                    result = "New Person Added";
+                }
+                else
+                {
+                    result="ERROR";
+                }
+
+                Toast toast = Toast.makeText(getApplicationContext(), result, duration);
                 toast.show();
+
+
+                fName.setText("");
+                lName.setText("");
+                email.setText("");
+                phone.setText("");
+                fName.requestFocus();
             }
         });
     }
@@ -65,23 +92,21 @@ public class AddPersonActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
 
-        if (id == R.id.menuAct1){
+        if (id == R.id.display){
             Intent intent = new Intent(getApplicationContext(), DisplayDb.class);
             startActivity(intent);
             return true;
         }
-        if (id == R.id.menuAct3) {
-            SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.clear();
-            editor.apply();
+        if (id == R.id.clearDb) {
+            PersonDbHelper myDbHelper = new PersonDbHelper(getApplicationContext());
+            SQLiteDatabase db = myDbHelper.getWritableDatabase();
+            db.delete(PersonContract.PersonEntry.TABLE_NAME, "1", null);
 
-            ArrayList<String> list = new ArrayList<>();
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.row_item, R.id.listItem, list);
-            final ListView listView = findViewById(android.R.id.list);
-            listView.setAdapter(adapter);
-            TextView emptyView = findViewById(android.R.id.empty);
-            listView.setEmptyView(emptyView);
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(getApplicationContext(), "Database cleared", duration);
+            toast.show();
+
             return true;
         }
 
--- app/src/main/java/com/example/charley/assignfive/DisplayDb.java
+++ app/src/main/java/com/example/charley/assignfive/DisplayDb.java
@@ -17,16 +17,18 @@ import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
+import static android.R.id.list;
+
 public class DisplayDb extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_db);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -55,14 +57,14 @@ public class DisplayDb extends AppCompatActivity {
 
         Cursor cursor = db.query(PersonContract.PersonEntry.TABLE_NAME, bind, null, null, null, null, PersonContract.PersonEntry.COLUMN_NAME_LAST + " ASC");
 
-        int [] to = new int[]{R.id.fName, R.id.lName, R.id.phone, R.id.email};
+        int [] to = new int[]{R.id.first, R.id.last, R.id.phone, R.id.email};
 
         SimpleCursorAdapter adapter  = new SimpleCursorAdapter(getApplicationContext(), R.layout.row_item, cursor, projection, to, 0);
 
-        final ListView listView = (ListView) findViewById(list);
+        final ListView listView = findViewById(list);
         listView.setAdapter(adapter);
 
-        TextView emptyView = (TextView) findViewById(android.R.id.empty);
+        TextView emptyView = findViewById(android.R.id.empty);
         listView.setEmptyView(emptyView);
 
 
@@ -73,7 +75,7 @@ public class DisplayDb extends AppCompatActivity {
 
                 Cursor cursor = (Cursor) parent.getItemAtPosition(position);
 
-                String selectedItem = (String) cursor.getString(cursor.getColumnIndex(PersonContract.PersonEntry.COLUMN_NAME_FIRST));
+                String selectedItem = cursor.getString(cursor.getColumnIndex(PersonContract.PersonEntry.COLUMN_NAME_FIRST));
 
                 Snackbar.make(view, selectedItem, Snackbar.LENGTH_LONG).setAction("Action", null).show();
 
